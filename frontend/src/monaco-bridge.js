b5cb45bd8a9c5435dd0b9be73af7f41f9cfd041b
NJgaoyang <hxhxnxhxbdb@gmail.com>
2026-09-11 10:56:03 +0800

    fix: load development permission controls before workspace actions

--- frontend/src/monaco-bridge.js
+++ frontend/src/monaco-bridge.js
@@ -2,6 +2,7 @@ import * as monaco from '../node_modules/monaco-editor/esm/vs/editor/editor.api.
 import editorWorker from '../node_modules/monaco-editor/esm/vs/editor/editor.worker.js?worker';
 import '../node_modules/monaco-editor/min/vs/editor/editor.main.css';
 import './integration-task-dialog-bootstrap.js';
+import './development-permissions.js';
 import './development-workspace.js';
 import './settings-cluster-fix.js';
 
